89523ba6f5dc95796e65ab0e870770087069c2a2
shantanu605 <85123041+shantanu605@users.noreply.github.com>
2022-02-24 19:26:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIAVFBR3CHWJZY2S3UV"
+  default = "AKIARFYZK35UCAPPFGO6"
   }
 variable "secret_key" {
-  default = "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"
+  default = "vZldoNqZDbjExUQC2QnPfkGrCI6qcOf/vcHka+sc"
   }
 variable "region" {
   default = "ap-south-1"
